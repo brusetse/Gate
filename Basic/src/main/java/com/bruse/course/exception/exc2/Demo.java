cfe9123339ef810387ed6696e08ecbe7037c4c5f
Bruce <brusetse@163.com>
2017-11-09 12:01:38 +0800

    fix bug

--- Basic/src/main/java/com/bruse/course/exception/exc2/Demo.java
+++ Basic/src/main/java/com/bruse/course/exception/exc2/Demo.java
@@ -1,5 +1,7 @@
 package com.bruse.course.exception.exc2;
 
+import org.junit.Test;
+
 import java.util.Scanner;
 
 public class Demo {
@@ -20,11 +22,33 @@ public class Demo {
 	static void isTriangle(int a,int b,int c) throws IllegalArgumentException{
 		if((a>=b+c)||(b>=a+c)||(c>=a+b))
 			throw new IllegalArgumentException("a,b,c不能构成三角形");
+
+	}
+
+	public void aaa() throws KobeException {
+		if (Math.random() < 0.5) {
+			throw new KobeException("蜗壳又打铁了");
+		}
+	}
+
+	@Test
+	public void test() {
+		try {
+			aaa();
+		} catch (KobeException e) {
+			System.out.println(e);
+		}
 	}
 }
 
 class IllegalArgumentException extends Exception{
 	IllegalArgumentException(String msg) {
 		super(msg);
+	}
+}
+
+class KobeException extends Exception {
+	public KobeException(String message) {
+		super(message);
 	}
 }
